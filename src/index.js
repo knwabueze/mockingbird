05e77526fd8edb4d7620b0d2ac14fd13c14cc856
Kosi Nwabueze <kosinwabueze@gmail.com>
2017-06-09 13:36:40 -0700

    Test Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,3 @@ import { createRouter } from './components/router';
 import createStores from './stores'
 
 ReactDOM.render(createRouter(createStores()), document.getElementById('root'));
-
-if (module.hot) {
-    module.hot.accept('./components/router', () => {
-        const newRouter = require('./components/router').default;
-        const newStores = require('./stores').default;
-        ReactDOM.render(newRouter(newStores))
-    });
-};
